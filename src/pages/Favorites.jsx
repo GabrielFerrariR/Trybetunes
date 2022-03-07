6875ed38a32c3ffaa6ac2bcbfb1b8a18b76bc61c
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-03-07 15:17:32 -0300

    REquisito 1 Crie as rotas necessárias para a aplicação

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">Favorista</div>
+    );
+  }
+}
+export default Favorites;
---
+++ src/pages/Search.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">Busca</div>
+    );
+  }
+}
+export default Search;
