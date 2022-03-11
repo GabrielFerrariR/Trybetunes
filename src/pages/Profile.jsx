18186ad6446b0e6c9fa6e9a8f2d063f087c7c526
Gabriel Ferrari Ribeiro <gabrielferrari601@gmail.com>
2022-03-11 19:16:29 -0300

    Adiciona o requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.userInfo();
+  }
+
+  userInfo = async () => {
+    const { description, email, image, name } = await getUser();
+    this.setState({
+      description,
+      email,
+      image,
+      name,
+    }, this.setState({ loading: false }));
+  }
+
   render() {
+    const { loading, description, email, image, name } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading && <Loading /> }
+        { !loading && (
+          <>
+            <img src={ image } alt="Profile" data-testid="profile-image" />
+            <Link to="/profile/edit">Editar perfil</Link>
+            <h2>Nome</h2>
+            <p>{name}</p>
+            <h2>Email</h2>
+            <p>{email}</p>
+            <h2>Descrição</h2>
+            <p>{description}</p>
+          </>) }
       </div>
     );
   }
